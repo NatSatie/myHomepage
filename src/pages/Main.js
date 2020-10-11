d13d6238a5311ab7a5e49dddf6e5253b654c8177
Nat <naty.odashima@gmail.com>
2020-10-11 17:10:48 -0300

    add icons

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,15 +1,7 @@
 import React from 'react'
-import { Grid, Image} from 'semantic-ui-react'
+import { Grid} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
-import avatar from '../img/profileSquare.png'
-import marker from '../img/maps-and-flags.svg'
 import '../index.css'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
 import Menu from '../components/Menu'
 import About from '../components/About'
 
@@ -17,13 +9,13 @@ class Main extends React.Component {
 
   render(){
     return(
-      <div>
-        <Grid columns={2} divided>
+      <div style={{padding: '0', margin: '0', backgroundColor: '#E6E5FB'}}>
+        <Grid columns={2}>
           <Grid.Row>
             <Grid.Column width={4}>
               <Menu/>
             </Grid.Column>
-            <Grid.Column width={12} style={{backgroundColor:'white'}}>
+            <Grid.Column width={12}>
               <About/>
             </Grid.Column>
           </Grid.Row>
